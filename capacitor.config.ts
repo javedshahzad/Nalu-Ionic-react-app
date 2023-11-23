f1ec84f64aeaf17a84ed638f2b955cad90b2962c
prosperofilipe <prospero@filipe.ch>
2023-11-23 01:41:17 +0100

    Android & iOS Build Preperation

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import { CapacitorConfig } from "@capacitor/cli";
 
 const config: CapacitorConfig = {
   appId: "com.nalu.app",
-  appName: "nalu",
+  appName: "NALU",
   webDir: "dist",
   server: {
     androidScheme: "https",
