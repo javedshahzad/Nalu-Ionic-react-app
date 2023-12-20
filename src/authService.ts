500e03b997db5a859b1b11a76d6ffcb44f082dc0
Arslan Azhar <arslan@arslans-mbp.home>
2023-12-20 21:56:39 +0500

    added authService

---
+++ src/authService.ts
@@ -0,0 +1,16 @@
+import { useHistory } from "react-router";
+
+const history = useHistory();
+
+const authService = {
+  logout: () => {
+    localStorage.removeItem("jwtToken");
+    sessionStorage.removeItem("jwtToken");
+    localStorage.removeItem("roles");
+    localStorage.removeItem("userId");
+
+    history.push("/login");
+  },
+};
+
+export default authService;
