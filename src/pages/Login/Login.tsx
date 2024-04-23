666592d864864eff5cb7049936d202e39d5c7ac4
Arslan <arslanazhar00@gmail.com>
2024-04-23 12:15:54 +0500

    changed url

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -118,7 +118,7 @@ const Login: React.FC = () => {
         }
 
         // Chat API login
-        let url = "https://app.mynalu.com";
+        let url = "https://apidev.mynalu.com";
         //  let url = 'http://localhost:7001'
         try {
           const naluApiResponse = await HTTP.post(
@@ -134,16 +134,16 @@ const Login: React.FC = () => {
             const { access, refresh, user } = naluApiResponseData.data.tokens;
             const wsToken = naluApiResponseData.data.wsToken;
             const chatUser = naluApiResponseData.data.user;
-        
+
             // Chat Backend Tokens
             localStorage.setItem("accessToken", access.token);
             localStorage.setItem("refreshToken", refresh.token);
             localStorage.setItem("chatApiUserId", user._id);
-        
+
             // Chat iFrame Tokens
             localStorage.setItem("chatToken", access.token); // This is the same as accessToken, stored under a new key
             localStorage.setItem("chatWsToken", wsToken);
-            
+
             // Chat iFrame User Information
             const chatUserString = JSON.stringify(chatUser);
             localStorage.setItem("chatUser", chatUserString);
@@ -160,7 +160,7 @@ const Login: React.FC = () => {
           // Handle any errors with the Chat API login here
           console.error("Chat API login error:", chatApiError);
         }
-        
+
         // Navigation
         history.push("/tabs/tab1");
       } else {
@@ -186,7 +186,7 @@ const Login: React.FC = () => {
           return;
         }
 
-       // Chat API login
+        // Chat API login
         try {
           const naluApiResponse = await axios.post(
             "https://apidev.mynalu.com/v1/user/login",
@@ -209,7 +209,7 @@ const Login: React.FC = () => {
             // Chat iFrame Tokens
             localStorage.setItem("chatToken", access.token); // This is the same as accessToken, stored under a new key
             localStorage.setItem("chatWsToken", wsToken);
-            
+
             // Chat iFrame User Information
             const chatUserString = JSON.stringify(chatUser);
             localStorage.setItem("chatUser", chatUserString);
